92a7136a68be6cf7397412621b3a6228874c615f
Azegba Kizito Amandianeze <kizitoamandi96@gmail.com>
2026-07-19 10:28:32 +0100

    Implemented interactive main menu

--- src/app.py
+++ src/app.py
@@ -1,3 +1,5 @@
+from menu import display_menu
+
 class VisionLab:
     def __init__(self):
         self.name = "VisionLab"
@@ -8,3 +10,20 @@ class VisionLab:
         print(f"Welcome to {self.name}")
         print(f"Version: {self.version}")
         print("=" *50)
+
+
+    while True:
+        display_menu
+
+        choice = input("Enter your choice (1-4): ")
+        if choice == "1":
+            print("Opening image... Coming soon!")
+        elif choice == "2":
+            print("Saving Image ... Coming Soon!")
+        elif choice == "3":
+            print("Image information ... Coming Soon!")
+        elif choice == "4":
+            print("Exiting the program. Goodbye!")
+            break
+        else:
+            print("Invalid choice. Please enter a number between 1 and 4.")
---
+++ src/menu.py
@@ -0,0 +1,11 @@
+def display_menu():
+    """Displays the main menu options to the user.
+    """
+    print("\n" + "=" * 50)
+    print("              VisionLab Main Menu")
+    print("=" * 50)
+    print("1. Open Image")
+    print("2. Save Image")
+    print("3. Image information")
+    print("4. Exit")
+    print("=" * 50)
